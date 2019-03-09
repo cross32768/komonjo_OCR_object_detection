8b456a91942320ed128471a3f1e087d7f6d16833
cross32768 <suzukikaito14.ne.jp@gmail.com>
2019-03-09 18:43:09 +0900

    update resize.py to decide output directory name in the program

--- resize.py
+++ resize.py
@@ -12,20 +12,19 @@ def main():
     parser = argparse.ArgumentParser(description='program to resize images')
     parser.add_argument('image_dir',
                         help='path to directory include original images')
-    parser.add_argument('output_dir',
-                        help='path to directory save resized images')
     parser.add_argument('resized_image_size',
-                        help='resized image size (only square)',
+                        help='resized image size (only square is supported)',
                         type=int)
     args = parser.parse_args()
 
     image_dir = normalize_dir_name(args.image_dir)
-    output_dir = normalize_dir_name(args.output_dir)
     resized_image_size = args.resized_image_size
+
+    output_dir = image_dir[:-1] + '_resized_' + str(resized_image_size) + '/'
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
-    assert len(os.listdir(output_dir)) == 0, 'Output directory is not empty.'
+    assert len(os.listdir(output_dir)) == 0, 'The output directory is not empty.'
 
     image_name_list = os.listdir(image_dir)
     for image_name in tqdm(image_name_list):
