ca36fd29d5acf092d2a5834ba2fcde6d86ff9c64
cross32768 <suzukikaito14.ne.jp@gmail.com>
2019-03-20 15:01:21 +0900

    update config for readability and update resize.py to conduct multiscale resizing for multiscale training

--- config.py
+++ config.py
@@ -1,39 +1,34 @@
 # data settings
 DATASET_ROOT_DIR = '../../data/komonjo/'
-DATASET_DIR_LIST = [DATASET_ROOT_DIR + '100241706/',
-                    DATASET_ROOT_DIR + '100249371/',
-                    DATASET_ROOT_DIR + '100249376/',
-                    DATASET_ROOT_DIR + '100249416/',
-                    DATASET_ROOT_DIR + '100249476/',
-                    DATASET_ROOT_DIR + '100249537/',
-                    DATASET_ROOT_DIR + '200003076/',
-                    DATASET_ROOT_DIR + '200003967/',
-                    DATASET_ROOT_DIR + '200004148/',
-                    DATASET_ROOT_DIR + '200005598/',
-                    DATASET_ROOT_DIR + '200006663/',
-                    DATASET_ROOT_DIR + '200014685/',
-                    DATASET_ROOT_DIR + '200014740/',
-                    DATASET_ROOT_DIR + '200015779/',
-                    DATASET_ROOT_DIR + '200021637/',
-                    DATASET_ROOT_DIR + '200021644/',
-                    DATASET_ROOT_DIR + '200021660/',
-                    DATASET_ROOT_DIR + '200021712/',
-                    DATASET_ROOT_DIR + '200021763/',
-                    DATASET_ROOT_DIR + '200021802/',
-                    DATASET_ROOT_DIR + '200021851/',
-                    DATASET_ROOT_DIR + '200021853/',
-                    DATASET_ROOT_DIR + '200021869/',
-                    DATASET_ROOT_DIR + '200021925/',
-                    DATASET_ROOT_DIR + '200022050/',
-                    DATASET_ROOT_DIR + 'brsk00000/',
-                    DATASET_ROOT_DIR + 'hnsd00000/',
-                    DATASET_ROOT_DIR + 'umgy00000/'
+DATASET_DIR_LIST = [DATASET_ROOT_DIR + '100241706/',  # 0
+                    DATASET_ROOT_DIR + '100249371/',  # 1
+                    DATASET_ROOT_DIR + '100249376/',  # 2
+                    DATASET_ROOT_DIR + '100249416/',  # 3
+                    DATASET_ROOT_DIR + '100249476/',  # 4
+                    DATASET_ROOT_DIR + '100249537/',  # 5
+                    DATASET_ROOT_DIR + '200003076/',  # 6
+                    DATASET_ROOT_DIR + '200003967/',  # 7
+                    DATASET_ROOT_DIR + '200004148/',  # 8
+                    DATASET_ROOT_DIR + '200005598/',  # 9
+                    DATASET_ROOT_DIR + '200006663/',  # 10
+                    DATASET_ROOT_DIR + '200014685/',  # 11
+                    DATASET_ROOT_DIR + '200014740/',  # 12
+                    DATASET_ROOT_DIR + '200015779/',  # 13
+                    DATASET_ROOT_DIR + '200021637/',  # 14
+                    DATASET_ROOT_DIR + '200021644/',  # 15
+                    DATASET_ROOT_DIR + '200021660/',  # 16
+                    DATASET_ROOT_DIR + '200021712/',  # 17
+                    DATASET_ROOT_DIR + '200021763/',  # 18
+                    DATASET_ROOT_DIR + '200021802/',  # 19
+                    DATASET_ROOT_DIR + '200021851/',  # 20
+                    DATASET_ROOT_DIR + '200021853/',  # 21
+                    DATASET_ROOT_DIR + '200021869/',  # 22
+                    DATASET_ROOT_DIR + '200021925/',  # 23
+                    DATASET_ROOT_DIR + '200022050/',  # 24
+                    DATASET_ROOT_DIR + 'brsk00000/',  # 25
+                    DATASET_ROOT_DIR + 'hnsd00000/',  # 26
+                    DATASET_ROOT_DIR + 'umgy00000/',  # 27
                     ]
-
-
-N_KINDS_OF_CHARACTERS = 10
-RESIZE_IMAGE_SIZE = 704
-RESIZE_IMAGE_SIZE_CANDIDATES = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672, 704]
 
 # model settings
 FE_STRIDE = 32
@@ -42,4 +37,7 @@ LAMBDA_COOR = 5.0
 LAMBDA_SIZE = 2.5
 
 # experiment settings
+N_KINDS_OF_CHARACTERS = 10
+RESIZE_IMAGE_SIZE = 512
+RESIZE_IMAGE_SIZE_CANDIDATES = [256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672, 704, 736, 768]
 RANDOM_SEED = 0
--- resize.py
+++ resize.py
@@ -5,26 +5,17 @@ from PIL import Image
 from tqdm import tqdm
 
 from utils import normalize_dir_name
+import config
 
 
-def main():
-
-    parser = argparse.ArgumentParser(description='program to resize images')
-    parser.add_argument('image_dir',
-                        help='path to directory include original images')
-    parser.add_argument('resized_image_size',
-                        help='resized image size (only square is supported)',
-                        type=int)
-    args = parser.parse_args()
-
-    image_dir = normalize_dir_name(args.image_dir)
-    resized_image_size = args.resized_image_size
-
+def resize_and_save(image_dir, resized_image_size):
     output_dir = image_dir[:-1] + '_resized_' + str(resized_image_size) + '/'
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
-    assert len(os.listdir(output_dir)) == 0, 'The output directory is not empty.'
+    if len(os.listdir(output_dir)) != 0:
+        print('The output directory is not empty.')
+        return 0
 
     image_name_list = os.listdir(image_dir)
     for image_name in tqdm(image_name_list):
@@ -32,6 +23,21 @@ def main():
         image = image.resize((resized_image_size, resized_image_size),
                              Image.LANCZOS)
         image.save(output_dir + image_name)
+    print(str(resized_image_size) + ' is completed.')
+    return 1
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='program to resize images')
+    parser.add_argument('image_dir',
+                        help='path to directory include original images')
+    args = parser.parse_args()
+
+    image_dir = normalize_dir_name(args.image_dir)
+    for resized_image_size in config.RESIZE_IMAGE_SIZE_CANDIDATES:
+        resize_and_save(image_dir, resized_image_size)
+
 
 if __name__ == '__main__':
     main()
